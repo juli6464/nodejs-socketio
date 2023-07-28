24d3478488998d327202bc94c08d953a85c197ba
juli6464 <juli6464@hotmail.com>
2023-07-28 09:06:33 -0500

    on, once y off

--- src/index.js
+++ src/index.js
@@ -11,11 +11,15 @@ const io = new Server(httpServer);
 
 app.use(express.static(path.join(__dirname, "views")));
 
+const socketsOnline = [];
+
 app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/views/index.html");
 });
 
 io.on("connection", socket => {
+
+    socketsOnline.push(socket.id);
 
     //EMISION BASICA
     socket.emit("welcome", "Ahora estás conectado.");
@@ -26,6 +30,31 @@ io.on("connection", socket => {
 
     //emisión a todos
     io.emit("everyone", socket.id + "se ha conectado");
+
+    //emisión a uno solo
+    socket.on("last", message => {
+
+        const lastSocket = socketsOnline[socketsOnline.length - 1];
+        io.to(lastSocket).emit("salute", message);
+
+    })
+
+    //on
+    // socket.emit("on", "hola");
+    // socket.emit("on", "hola");
+
+    //once
+    // socket.emit("once", "hola");
+    // socket.emit("once", "hola");
+
+    //off
+    socket.emit("off", "hola");
+
+    setTimeout(() => {
+        socket.emit("off", "hola");
+    }, 3000);
+
+
 
 });
 
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -15,3 +15,33 @@ emitToServer.addEventListener("click", () => {
 socket.on("everyone", message => {
     console.log(message);
 });
+
+const emitToLast = document.querySelector("#emit-to-last");
+emitToLast.addEventListener("click", () => {
+    socket.emit("last", "hola ")
+});
+
+socket.on("salute", message => {
+    console.log(message);
+});
+
+//on se emite varias veces
+socket.on("on", () => {
+    console.log("se emite varias veces");
+});
+
+//once se emite una vez
+socket.once("once", () => {
+    console.log("se emite una sola vez");
+});
+
+//funcion anonima que paso al off
+const listener = () => {
+    console.log("se apaga el evento");
+}
+
+socket.on("off", listener);
+
+setTimeout(() => {
+    socket.off("off", listener);
+}, 2000);
