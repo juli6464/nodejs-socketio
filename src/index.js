762747e49a527f861879e0f379205f71b6ab630f
juli6464 <juli6464@hotmail.com>
2023-08-04 14:04:41 -0500

    eventos volatiles

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const path = require("path");
 const { createServer } = require("http");
-const { Server} = require("socket.io");
-const { Socket } = require("dgram");
+const { Server } = require("socket.io");
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
-
-//este index.js es el servidor
 
 app.use( express.static(path.join(__dirname, "views")) );
 
@@ -16,15 +13,12 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
 
-
 io.on("connection", socket => {
 
-    socket.on("is connected", msg => {
-        console.log(msg);
+    socket.on("circle position", position => {
+        socket.broadcast.emit("move circle", position);
     });
 
 });
-
-
 
 httpServer.listen(3000);
