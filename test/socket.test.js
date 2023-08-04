690017339176729c74bd8dc79da94d56241a3318
juli6464 <juli6464@hotmail.com>
2023-08-04 15:32:51 -0500

    Configuración para testing usando la librería jest

---
+++ test/socket.test.js
@@ -0,0 +1,37 @@
+const { create } = require("domain");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const Client = require("socket.io-client");
+
+describe("Testing Socket.io", () => {
+
+    let io, serverSocket, clientSocket;
+
+    //antes de empezar a hacer los test, creamos el servidor
+    beforeAll(done => {
+
+        const httpServer = createServer();
+        io = new Server(httpServer);
+
+        httpServer.listen(() => {
+
+            const port = httpServer.address().port;
+            clientSocket = new Client(`http://localhost:${port}`);
+
+            io.on("connection", socket => {
+                serverSocket = socket;
+            });
+
+            clientSocket.on("connect", done);
+
+        });
+        
+    });
+
+    afterAll(() => {
+        io.close();
+        clientSocket.close();
+    });
+    
+});
+
